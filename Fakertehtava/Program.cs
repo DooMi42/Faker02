516abf4e8b8c7d5f0de27ac9dba674310fc5d006
DooMi <hurmerintaeemeli@gmail.com>
2022-09-13 17:10:37 +0300

    Initial commit

---
+++ Fakertehtava/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Fakertehtava
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            var name = Faker.Name.FullName();
+            var address = Faker.Address.StreetAddress();
+            var city = Faker.Address.City();
+            var number = Faker.RandomNumber.Next(100);
+            var dob = Faker.Identification.DateOfBirth();
+            var usPassport = Faker.Identification.UsPassportNumber();
+
+            Console.WriteLine($"First person: {name} - {address} - {city} - {number} - {dob} - {usPassport}");
+
+            var name3 = Faker.Name.FullName();
+            var address3 = Faker.Address.StreetAddress();
+            var city3 = Faker.Address.City();
+            var number3 = Faker.RandomNumber.Next(100);
+            var dob3 = Faker.Identification.DateOfBirth();
+            var ukPassport3 = Faker.Identification.UkPassportNumber();
+
+            Console.WriteLine($"Second person: {name3} - {address3} - {city3} - {number3} - {dob3} - {ukPassport3}");
+
+            var name2 = Faker.Name.FullName();
+            var address2 = Faker.Address.StreetAddress();
+            var city2 = Faker.Address.City();
+            var number2 = Faker.RandomNumber.Next(100);
+            var dob2 = Faker.Identification.DateOfBirth();
+            var usPassport2 = Faker.Identification.UsPassportNumber();
+
+            Console.WriteLine($"Third person: {name2} - {address2} - {city2} - {number2} - {dob2} - {usPassport2}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace Fakertehtava
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Adding matrix text color ;)
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            //Introduction to the program
+            #region
+
+            Console.WriteLine("Welcome to the program! Read the instructions below.\n" +
+                "\nUse command line argument: --personcount and press enter here.\n" +
+                "\nOr\n" +
+                "\nUse command line argument: --interactive and Type: --interactive to use the program.\n");
+            Console.Write("Type here: ");
+            string first = Console.ReadLine();
+            #endregion
+
+            //Personcount code
+            #region
+            if (args.Length == 0)
+            {
+                Console.WriteLine("No command line parameters given, Please enter parameter.");
+                return;
+            }
+            string firstParameter = args[0];
+
+            if (firstParameter == "--personCount" || firstParameter == "--personcount")
+            {
+                if (args.Length == 1)
+                {
+                    Console.WriteLine("ERROR: specify personcount value in command line argument.");
+                    return;
+                }
+
+                int howMany = Convert.ToInt32(args[1]);
+
+                Console.WriteLine("\nGiving you " + howMany + " fake persons data ENJOY!");
+
+                for (int i = 0; i < howMany; i++)
+                {
+                    FakerMethod();
+                }
+            }
+            #endregion
+
+            //Interactive code
+            #region
+            if (first == "--interActive" || first == "--interactive")
+            {
+                Console.WriteLine("\nHow many fake persons data you want?");
+
+                int amount = Convert.ToInt32(Console.ReadLine());
+
+                for (int i = 0; i < amount; i++)
+                {
+                    FakerMethod();
+                }
+            }
+            #endregion
+        }
+
+        //FakerMethod
+        #region
+        private static void FakerMethod()
+        {
+            Console.WriteLine("");
+
+            var name = Faker.Name.FullName();
+            var address = Faker.Address.StreetAddress();
+            var city = Faker.Address.City();
+            var number = Faker.RandomNumber.Next(100);
+            var dob = Faker.Identification.DateOfBirth();
+            var usPassport = Faker.Identification.UsPassportNumber();
+
+            Console.WriteLine($"Person: {name} - {address} - {city} - {number} - {dob} - {usPassport}");
+        }
+        #endregion
+    }
+}
